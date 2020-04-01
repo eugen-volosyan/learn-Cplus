c55c0be1f037b1e02ebe3c4e8c1504faa08ed7c5
eugen-volosyan <volosyan.eugen@gmail.com>
2020-04-01 12:03:23 +0300

    Initial Commit

---
+++ learnC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnC
+//
+//  Created by Eugen on 01.04.2020.
+//  Copyright © 2020 Eugen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
