300506f6143f1fe41d371187fecc5c26dd04ae90
eugen-volosyan <volosyan.eugen@gmail.com>
2020-04-01 13:26:12 +0300

    lesson 2

--- learnC/main.cpp
+++ learnC/main.cpp
@@ -12,4 +12,15 @@ int main (){
     int a=2, b=3, c;
     c = a+b;
     cout << c << endl;
+    
+    //lesssson 2//
+    double d = 1.234567;
+    cout.precision(2);
+    cout.width(20);
+    cout << d << endl;
+    cout.precision(6);
+    cout << d << endl;
+    cout.width(20);
 }
+
+
